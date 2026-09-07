499457991ac72b8cf22299dd1bcbc14004d0a477
Bosyaf <boss104yaf@gmail.com>
2026-09-08 01:42:57 +0300

    Rename module_destroy_params to destroy_params

--- kernel/module/main.c
+++ kernel/module/main.c
@@ -1190,7 +1190,7 @@ static void free_module(struct module *mod)
 	module_unload_free(mod);
 
 	/* Free any allocated parameters. */
-	module_destroy_params(mod->kp, mod->num_kp);
+	destroy_params(mod->kp, mod->num_kp);
 
 	if (is_livepatch_module(mod))
 		free_module_elf(mod);
@@ -2936,7 +2936,7 @@ static int load_module(struct load_info *info, const char __user *uargs,
 	mod_sysfs_teardown(mod);
  coming_cleanup:
 	mod->state = MODULE_STATE_GOING;
-	module_destroy_params(mod->kp, mod->num_kp);
+	destroy_params(mod->kp, mod->num_kp);
 	blocking_notifier_call_chain(&module_notify_list,
 				     MODULE_STATE_GOING, mod);
 	klp_module_going(mod);
